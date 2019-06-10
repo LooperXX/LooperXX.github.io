f778c363f88bf29d2841831cd9b94f54681e9e4e
LooperXX <looperxx@outlook.com>
2019-06-10 14:55:28 +0800

    update

---
+++ docs/js/baidu-tongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5b7580d38c0a05a6bd4bce74966d511d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
