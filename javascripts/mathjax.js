b5363267108b5b0333ab7c184293e7557256f4c6
LooperXX <looperxx@outlook.com>
2023-02-10 03:06:34 +0800

    update

--- javascripts/mathjax.js
+++ javascripts/mathjax.js
@@ -1,9 +1,28 @@
+// https://docs.mathjax.org/en/latest/input/tex/extensions/autoload.html
+
 window.MathJax = {
     tex: {
       inlineMath: [["\\(", "\\)"]],
       displayMath: [["\\[", "\\]"]],
       processEscapes: true,
-      processEnvironments: true
+      processEnvironments: true,
+      autoload: expandable({
+        action: ['toggle', 'mathtip', 'texttip'],
+        amscd: [[], ['CD']],
+        bbox: ['bbox'],
+        boldsymbol: ['boldsymbol'],
+        braket: ['bra', 'ket', 'braket', 'set', 'Bra', 'Ket', 'Braket', 'Set', 'ketbra', 'Ketbra'],
+        cancel: ['cancel', 'bcancel', 'xcancel', 'cancelto'],
+        color: ['color', 'definecolor', 'textcolor', 'colorbox', 'fcolorbox'],
+        enclose: ['enclose'],
+        extpfeil: ['xtwoheadrightarrow', 'xtwoheadleftarrow', 'xmapsto',
+                   'xlongequal', 'xtofrom', 'Newextarrow'],
+        html: ['href', 'class', 'style', 'cssId'],
+        mhchem: ['ce', 'pu'],
+        newcommand: ['newcommand', 'renewcommand', 'newenvironment', 'renewenvironment', 'def', 'let'],
+        unicode: ['unicode'],
+        verb: ['verb']
+      }),
     },
     options: {
       ignoreHtmlClass: ".*|",
